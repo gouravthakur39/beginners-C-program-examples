b88572d4f8638598f7b388770958b127a4685b64
gourav thakur <gourav.thakur39@gmail.com>
2018-10-03 19:58:41 +0530

    Update Factorial.c

--- Factorial.c
+++ Factorial.c
@@ -1,13 +1,15 @@
-include <stdio.h>
+#include <stdio.h>
 int main()
 {
     int n, i;
     unsigned long long factorial = 1;
+    //unsigned long long is the same as unsigned long long int. 
+    //Its size is platform-dependent, but guaranteed by the C standard (ISO C99) to be at least 64 bits. 
 
     printf("Enter a number: ");
     scanf("%d",&n);
 
-    // SHow error if number is less than 100
+    // Show error if number is less than 0
     if (n < 0)
         printf("Error! Factorial of a negative number doesn't exist.");
 
