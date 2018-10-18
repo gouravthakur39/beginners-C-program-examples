f263ebc5e70e8a4c6d411eb025dede13f0299960
Anirudh Bhat <anirudh.b.123@gmail.com>
2018-10-18 17:26:53 +0530

    Added prime number program

---
+++ Prime.c
@@ -0,0 +1,28 @@
+//A c program to print prime numbers from 1 to n
+
+#include<stdio.h>
+
+void main(){
+    int num, i, j;
+    printf("Enter number: ");
+    scanf("%d",&num);
+    printf("Prime numbers are:\n");
+    //traversing all numbers from 2 to entered integer num
+    for(i=2; i<num; i++)
+    {
+        //loop to check divisibility
+        for(j=2; j<i; j++)
+        {
+            //checking if it is divisible by any smaller number
+            if(i%j == 0)
+            {
+                //if it is divisible exiting from loop
+                break;
+            }
+        }
+        //printing the number if it is never divisible by numbers smaller than itself
+        if(i == j){
+            printf("%d\n", i);
+        }
+    }
+}
