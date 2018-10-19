8557e4026670d076334e8e4cdb90c1b6aea6af7f
prince2580 <43826321+prince2580@users.noreply.github.com>
2018-10-19 18:30:18 +0545

    update swap values without using third variable

--- SwapValueWithoutUsingThirdVariable.c
+++ SwapValueWithoutUsingThirdVariable.c
@@ -7,9 +7,9 @@ int main()
     int a, b;
     printf("Enter two no :\n");
     scanf("%d%d",&a,&b);
-    a = a+b;
-    b = a-b;
-    a=  a-b;
+    a = a^b;
+    b = a^b;
+    a=  a^b;
     printf("After swapping value of a and b : %d,%d",a,b);
     return 0;
 }
