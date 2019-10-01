6239af8bfdb24b49e343194da62afc54d4845af9
geetanjaliaich <aichgeetanjali@gmail.com>
2019-10-01 23:53:06 +0530

    All 3 programs updated and commented

--- PrimeByEratosthenes.c
+++ PrimeByEratosthenes.c
@@ -5,22 +5,22 @@
  
 int main(){
     unsigned long long int i,j;
-    int *primes;
+    int *primes;				//pointer created for prime
     int z = 1;
  
-    primes = malloc(sizeof(int) * limit);
+    primes = malloc(sizeof(int) * limit);	//allocating 100 spaces in memory with the starting address to prime pointer
  
-    for (i = 2;i < limit; i++)
+    for (i = 2;i < limit; i++)			//initializing the 100 memory spaces
         primes[i] = 1;
  
-    for (i = 2;i < limit; i++)
+    for (i = 2;i < limit; i++)			//Filling the array with prime numbers as per the method proposed by seive of eratosthenes
         if (primes[i])
             for (j = i;i * j < limit; j++)
                 primes[i * j] = 0;
  
     printf("\nPrime numbers in range 1 to 100 are: \n");
 
-    for (i = 2;i < limit; i++)
+    for (i = 2;i < limit; i++)			//Printing the prime numbers in the range from 1 to 100
         if (primes[i])
             printf("%d\n", i);
  
