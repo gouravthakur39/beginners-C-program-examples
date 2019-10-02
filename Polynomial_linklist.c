aad7f0db75b7bf7f20bfcb7deca82e075dd35702
Sourov Sarkar <42178766+Leoperon@users.noreply.github.com>
2019-10-02 22:37:52 +0530

    update polynomial linklist
    Removed Syntaxerror

--- Polynomial_linklist.c
+++ Polynomial_linklist.c
@@ -14,28 +14,28 @@ void create() {
   printf("Enter each term with coeff and exp\n");
   for (i = 0; i < num; i++) {
     t = (struct Node * ) malloc(sizeof(struct Node));
-    scanf("%d%d", & t - > coeff, & t - > exp);
-    t - > next = NULL;
+    scanf("%d%d", &t->coeff, &t->exp);
+    t-> next = NULL;
     if (poly == NULL) {
       poly = last = t;
     } else {
-      last - > next = t;
+      last -> next = t;
       last = t;
     }
   }
 }
 void Display(struct Node * p) {
   while (p) {
-    printf("%dx%d +", p - > coeff, p - > exp);
-    p = p - > next;
+    printf("%dx%d +", p -> coeff, p -> exp);
+    p = p -> next;
   }
   printf("\n");
 }
 long Eval(struct Node * p, int x) {
   long val = 0;
   while (p) {
-    val += p - > coeff * pow(x, p - > exp);
-    p = p - > next;
+    val += p -> coeff * pow(x, p -> exp);
+    p = p -> next;
   }
   return val;
 }
