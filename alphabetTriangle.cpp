88572410882574026a53208ea1291ea2e18bca44
Salil Cuncoliencar <Salil_Cuncoliencar@intuit.com>
2019-10-02 20:19:00 +0530

    Alphabet traingle C program

---
+++ alphabetTriangle.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>    
+#include<stdlib.h>  
+
+/*
+     A                                                                                                                                                        
+    ABA                                                                                                                                                       
+   ABCBA                                                                                                                                                      
+  ABCDCBA                                                                                                                                                     
+ ABCDEDCBA 
+ 
+*/
+int main(){  
+  int ch=65;    
+    int i,j,k,m;    
+    for(i=1;i<=5;i++)    
+    {    
+        for(j=5;j>=i;j--)    
+            printf(" ");    
+        for(k=1;k<=i;k++)    
+            printf("%c",ch++);    
+            ch--;    
+        for(m=1;m<i;m++)    
+            printf("%c",--ch);    
+        printf("\n");    
+        ch=65;    
+    }    
+return 0;  
+} 
