1e7acc576e219dac7347dbdc0ab1d5d669dacaae
Pulkit555 <pulkit.g@students.iiit.ac.in>
2019-10-07 00:10:29 +0530

    removed extra binary file

--- PrimalityTest.c
+++ PrimalityTest.c
@@ -3,10 +3,12 @@
 int main()
 {
   int T,N,a;
+  printf("Enter the no. of test cases\n");
   scanf("%d",&T);//input the no. of test cases 
   for(int i=0;i<T;i++)
   {
     int k=0;
+    printf("Enter the no. you want to check whether it is prime or not\n");
 	  scanf("%d",&N);//input the no. you want to check
     for(int j=2;j<sqrt(N);j++)
     {
