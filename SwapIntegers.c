696b9e7182a00eece989c2ca90233b4ecb683989
RichaSinghiiit <richa.cse.iiit@gmail.com>
2019-10-07 23:48:00 +0530

    Swapped two integers using a third variable

--- SwapIntegers.c
+++ SwapIntegers.c
@@ -1,15 +1,20 @@
+
+//Swap two integers using a third variable
+
+
 #include <stdio.h>
 int main()
 {
-	int num1=5, num2=10;
-	
-	printf("The numbers are: %d and %d", &num1,&num2);
-	
-	num1=num1+num2; //num1=15 and num2=10
-	num2=num1-num2; //num1=15 and num2=5
-	num1=num1-num2; //num1=10 and num2=5
-
-	printf("The numbers have been swapped with new positions: %d and %d", &num1,&num2);
-
-	return 0;
+        int num1, num2, var;
+        printf("Enter two numbers to be swapped : ");
+        scanf(" %d %d", &num1, &num2);
+        var = num1;
+        num1 = num2;
+        num2 = var;
+        printf("Numbers after swapping are %d and %d.\n", num1, num2);
+        return 0;
 }
+
+
+
+
