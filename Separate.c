51bbbec6866369248e713999a0207eca8f4f6a11
Aman Khandelwal <khandelwalaman28@gmail.com>
2019-10-09 01:43:33 +0530

    Basic_Examples

---
+++ Separate.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{ float a,c;
+ int b;
+printf("Enter the Number : ");
+scanf("%f",&a);
+b=a/1;
+c=a-b;
+printf("Integer Part is : %d\n",b);
+printf("Decimal Part is : %f",c);
+}
---
+++ Subtraction.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{ int a,b,c;
+printf("Enter two Numbers : ");
+scanf("%d %d",&a,&b);
+c=a-b;
+printf("Subtraction is %d",c);
+}
