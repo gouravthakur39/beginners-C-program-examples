fc528007c65974cd615797f5615ec70cda769a77
lavanya28091999 <lavanyavcet1999@gmail.com>
2019-10-13 11:44:33 +0530

    addition edited

--- Addition.c
+++ Addition.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
 void main()
-{ int a,b,c;
+{ 
+int a,b,c;
 printf("Enter two Numbers : ");
 scanf("%d %d",&a,&b);
+if (c>5)
+{
 c=a+b;
 printf("Sum is %d",c);
 }
+else
+{
+printf("Error")
+}}
