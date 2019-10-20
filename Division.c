341dc1aa48154dd404bc9934b32e2d16a99b8ea5
Nischitha9 <50223199+Nischitha9@users.noreply.github.com>
2019-10-20 15:13:04 +0530

    Update Division.c

--- Division.c
+++ Division.c
@@ -2,7 +2,13 @@
 void main()
 { int a,b,c;
 printf("Enter two Numbers : ");
-scanf("%d %d",&a,&b);
-c=a/b;
+scanf("%d %d",&b,&c);
+if(b>c)
+{a=b/c;
 printf("Division is %d",c);
+}
+else
+{
+  printf("error");
+}
 }
