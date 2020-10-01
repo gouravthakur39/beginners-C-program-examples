78480a46bc3b5d1d0d596991e2ede734d247d686
sakshi singh <59533507+sakshi-singh-1011@users.noreply.github.com>
2020-10-01 08:11:08 +0530

    Update Addition.c

--- Addition.c
+++ Addition.c
@@ -1,8 +1,10 @@
+//Program to print addition of two numbers.
 #include<stdio.h>
 void main()
-{ int a,b,c;
-printf("Enter two Numbers : ");
-scanf("%d %d",&a,&b);
-c=a+b;
-printf("The Sum is %d",c);
+{ 
+  int a,b,c;                       //Declaring variables a,b and c as integer type
+  printf("Enter two Numbers : ");
+  scanf("%d %d",&a,&b);             //taking values of a and b as input from the user.
+  c=a+b;                            //adding a and b , stor
+  printf("The Sum is %d",c);
 }
