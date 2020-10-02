47ce61b801307d398af224bb1d1a1cf7132b197b
rajib <rm10078@gmail.com>
2020-10-02 09:14:22 +0530

    Your variabel num1 and num2 now show in output.

--- BasicArithmatic.c
+++ BasicArithmatic.c
@@ -4,18 +4,20 @@
 
 int main() {
    int number1, number2, addition, subtraction, multiplication, division, modulo;
-   printf("Enter two numbers :\n");
-   scanf("%d%d", & number1, & number2);
+   printf("Enter first : ");
+   scanf("%d",&number1);
+   printf("Second number : ",&number2);
+   scanf("%d",&number2);
    addition = number1 + number2;
    subtraction = number1 - number2;
    multiplication = number1 * number2;
    division = number1 / number2;
    modulo = number1 % number2;
-   printf("Addition of number 1 and number 2 : %d\n", addition);
-   printf("Subtraction of number 1 and number 2 : %d\n", subtraction);
-   printf("Multiplication of number 1 and number 2 : %d\n", multiplication);
-   printf("Division of number 1 and number 2 : %d\n", division);
-   printf("Modulo of number 1 and number 2 : %d\n", modulo);
+   printf("Addition of %d and %d : %d\n",number1,number2, addition);
+   printf("Subtraction of %d and %d : %d\n",number1,number2, subtraction);
+   printf("Multiplication of %d and %d : %d\n",number1,number2, multiplication);
+   printf("Division of %d and %d : %d\n",number1,number2, division);
+   printf("Modulo of %d and %d : %d\n",number1,number2, modulo);
    return 0;
 
 }
