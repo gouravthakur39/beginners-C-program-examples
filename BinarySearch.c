a6cd8da21c1498ef8d70f15a027befabb99572df
armishapadariya <66558691+armishapadariya@users.noreply.github.com>
2020-10-03 16:10:57 +0530

    Update BinarySearch.c

--- BinarySearch.c
+++ BinarySearch.c
@@ -1,46 +1,43 @@
-#include <stdio.h>
+// C program to implement recursive Binary Search 
+#include <stdio.h> 
 
-/* If x in into array return 0, else 1 */
-/* 
- * A function with 4 params:
- * int[] = array of elements
- * int = wanted number
- * int = start index
- * int = end index
- *
- * returns 0 or 1
- */
-int binarySearch(int[], int, int, int);
+// A recursive binary search function. It returns 
+// location of x in given array arr[l..r] is present, 
+// otherwise -1 
+int binarySearch(int arr[], int lft, int rgt, int x) 
+{ 
+	if (r >= l) { 
+		int mid = l + (r - l) / 2;   
 
-int main() {
-	int arr[] = {5, 15, 24, 32, 56, 89};
-    /* check length of array */
-	int size_of_array = sizeof(arr) / sizeof(int);
-	/* Check if 24 is into arr */
-	printf("%d\n", binarySearch(arr, 24, 0, size_of_array-1));
-	/* Check if 118 is into arr */
-	printf("%d\n", binarySearch(arr, 118, 0, size_of_array-1));
-	return 0;
-}
+		// If the element is present at the middle 
+		// itself 
+		if (arr[mid] == x) 
+			return mid; 
 
-int binarySearch(int array[], int number, int start, int end) {
-    /* if start index is get end index, check if that element is equals wanter nmber */
-	if(start >= end) {
-		return array[start] == number ? 0 : 1;
-	}
+		// If element is smaller than mid, then 
+		// it can only be present in left subarray 
+		if (arr[mid] > x) 
+			return binarySearch(arr, lft, mid - 1, x); 
 
-	int tmp = (int) end / 2;
-    /* divide array length in half */
-    /* if number is greater than element in half, do search by start to tmp
-	 * else search by tmp to end
-     */
-	if(number == array[tmp]) {
-		return 0;
-	} else if(number > array[tmp]) {
-		return binarySearch(array, number, start, tmp);
-	} else {
-		return binarySearch(array, number, tmp, end);
-	}
-}
+		// Else the element can only be present 
+		// in right subarray 
+		return binarySearch(arr, mid + 1, rgt, x); 
+	} 
 
+	// We reach here when element is not 
+	// present in array 
+	return -1; 
+} 
+
+int main(void) 
+{ 
+	int arr[] = { 2, 3, 4, 10, 40 }; 
+	int n = sizeof(arr) / sizeof(arr[0]); 
+	int x = 10; 
+	int result = binarySearch(arr, 0, n - 1, x); 
+	(result == -1) ? printf("Element is not present in array") 
+				: printf("Element is present at index %d", 
+							result); 
+	return 0; 
+} 
 
