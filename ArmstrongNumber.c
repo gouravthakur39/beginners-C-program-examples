b5a69339456faf9b3124740889401b9f2831f79e
lazy-dude <76253503+lazy-dude@users.noreply.github.com>
2020-12-26 18:48:13 +0330

    Update ArmstrongNumber.c

--- ArmstrongNumber.c
+++ ArmstrongNumber.c
@@ -5,27 +5,32 @@
 #include<stdio.h>
 #include<math.h>
 
-  void main() {
+int main(void)
+{
     int number, sum = 0, rem = 0, nthPower = 0, digits = 0, temp;
-    printf("Enter a number");
-    scanf("%d", & number);
+    printf("Enter a number:\n");
+    scanf("%d", &number);
     temp = number;
     //to calculate the number of digits in the number
     while (number != 0) {
-      number = number / 10;
-      digits++;
+	number = number / 10;
+	digits++;
     }
     number = temp;
     //to get the nth power of each digit and add it to the sum
     while (number != 0) {
-      rem = number % 10;
-      nthPower = pow(rem, digits);
-      sum = sum + nthPower;
-      number = number / 10;
+	rem = number % 10;
+	nthPower = pow(rem, digits);
+	sum = sum + nthPower;
+	number = number / 10;
     }
     //to check if obtained sum is equal to the original number
     if (sum == temp)
-      printf("The given number is an Armstrong number");
+	printf("The given number is an Armstrong number");
     else
-      printf("The given number is not an Armstrong number");
-  }
+	printf("The given number is not an Armstrong number");
+    printf("\n");
+    
+    return 0;	
+
+}
