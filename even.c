4d29c932a0dbf12218f395137a9cbd69393d347c
Sooraj2002 <jhasooraj56@gmail.com>
2021-01-07 23:06:08 +0530

    New c program to get Even No

---
+++ even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i;
+printf("\n ENTER THE VALUE OF N: ");
+scanf("%d",&n);
+printf("\n FIRST %d EVEN NUMBERS ARE: ");
+for(i=0;i<(2*n);i++)
+{
+printf("\n %d");
+i++;
+}
+getch();
+}
+
+
