b73331c389002156921fb6c35798cf392c3b6306
lazy-dude <76253503+lazy-dude@users.noreply.github.com>
2021-01-11 21:10:16 +0330

    Update Factorial.c

--- Factorial.c
+++ Factorial.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
-int main()
+#include <stdint.h>
+
+int main(void)
 {
     int n, i;
-    unsigned long long factorial = 1;
-    //unsigned long long is the same as unsigned long long int. 
-    //Its size is platform-dependent, but guaranteed by the C standard (ISO C99) to be at least 64 bits. 
+    uintmax_t factorial = 1;	// max width unsigned since C99
 
+    //printf("size is %zu\n",sizeof(uintmax_t));
     printf("Enter a number: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
     // Show error if number is less than 0
     if (n < 0)
-        printf("Error! Factorial of a negative number doesn't exist.");
+	printf("Error! Factorial of a negative number doesn't exist.");
 
-    else
-    {
-        for(i=1; i<=n; ++i)
-        {
-            factorial *= i;              // factorial = factorial*i;
-        }
-        printf("Factorial of %d = %llu", n, factorial);
+    else {
+	for (i = 1; i <= n; ++i) {
+	    factorial *= i;	// factorial = factorial*i;
+	}
+	printf("Factorial of %d = %ju\n", n, factorial);
     }
 
     return 0;
