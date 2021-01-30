ee214aa7d9367559c0d2e569b6ed2bb3f851681b
lazy-dude <76253503+lazy-dude@users.noreply.github.com>
2021-01-30 19:40:51 +0330

    Update PrimeByEratosthenes.c

--- PrimeByEratosthenes.c
+++ PrimeByEratosthenes.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
-#define limit 100 /*size of integers array*/
- 
-int main(){
-    unsigned long long int i,j;
-    int *primes;				//pointer created for prime
-    int z = 1;
- 
-    primes = malloc(sizeof(int) * limit);	//allocating 100 spaces in memory with the starting address to prime pointer
- 
-    for (i = 2;i < limit; i++)			//initializing the 100 memory spaces
-        primes[i] = 1;
- 
-    for (i = 2;i < limit; i++)			//Filling the array with prime numbers as per the method proposed by seive of eratosthenes
-        if (primes[i])
-            for (j = i;i * j < limit; j++)
-                primes[i * j] = 0;
- 
-    printf("\nPrime numbers in range 1 to 100 are: \n");
 
-    for (i = 2;i < limit; i++)			//Printing the prime numbers in the range from 1 to 100
-        if (primes[i])
-            printf("%d\n", i);
- 
-return 0;
+#define LIMIT 100		/*size of integers array */
+
+int main(void)
+{
+    unsigned long long int i, j;
+    int *primes;		//pointer created for prime
+    //int z = 1;
+
+    primes = malloc(sizeof(int) * LIMIT);	//allocating 100 spaces in memory with the starting address to prime pointer
+
+    for (i = 2; i < LIMIT; i++)	//initializing the 100 memory spaces
+	primes[i] = 1;
+
+    for (i = 2; i < LIMIT; i++)	//Filling the array with prime numbers as per the method proposed by seive of eratosthenes
+	if (primes[i])
+	    for (j = i; i * j < LIMIT; j++)
+		primes[i * j] = 0;
+
+    printf("\nPrime numbers in range 1 to %u are: \n", LIMIT);
+
+    for (i = 2; i < LIMIT; i++)	//Printing the prime numbers in the range from 1 to 100
+	if (primes[i])
+	    printf("%llu\n", i);
+
+    return 0;
 }
