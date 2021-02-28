ab52bf52e6ace6b3939df15b51a2538ead38b23e
lazy-dude <76253503+lazy-dude@users.noreply.github.com>
2021-02-28 19:07:56 +0330

    Update VowelorConsonant.c

--- VowelorConsonant.c
+++ VowelorConsonant.c
@@ -1,11 +1,16 @@
 // Program to input a character and check whether it is vowel or consonant using switch case
 #include <stdio.h>
-int main(){
+#include <ctype.h>
+#include <assert.h>
+
+int main(void)
+{
     char ch;
     printf("Enter a character\n");
     scanf("%c", &ch);
-    switch(ch)
-    {
+    assert(isalpha(ch));
+
+    switch (ch) {
     case 'a':
     case 'e':
     case 'i':
@@ -16,10 +21,10 @@ int main(){
     case 'I':
     case 'O':
     case 'U':
-        printf("Entered character is a vowel");
-        break;
+	printf("Entered character is a vowel\n");
+	break;
     default:
-        printf("Entered character is a consonant");
+	printf("Entered character is a consonant\n");
 
     }
 
