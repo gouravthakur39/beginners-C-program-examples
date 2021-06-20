d2c67d0449862aaa88e5e4701a3a712255bbc6c4
lazy-dude <76253503+lazy-dude@users.noreply.github.com>
2021-06-20 12:01:11 +0430

    Update Separate.c

--- Separate.c
+++ Separate.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
-void main()
-{ float a,c;
- int b;
-printf("Enter the Number : ");
-scanf("%f",&a);
-b=a/1;
-c=a-b;
-printf("Integer Part is : %d\n",b);
-printf("Decimal Part is : %f",c);
+int main(void)
+{ 
+	float a,c;
+	int b;
+	printf("Enter the Number : ");
+	scanf("%f",&a);
+	b=a/1;
+	c=a-b;
+	printf("Integer Part is : %d\n",b);
+	printf("Decimal Part is : %f\n",c);
+
+	return 0;
 }
