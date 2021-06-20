d7ee4ed2a4677b4e2e4d5a3b0827848b0c22f52d
lazy-dude <76253503+lazy-dude@users.noreply.github.com>
2021-06-20 12:15:35 +0430

    Update Swap_2.c

--- Swap_2.c
+++ Swap_2.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
-void main()
-{ int a,b;
-printf("Enter two Numbers : ");
-scanf("%d %d",&a,&b);
-a=a+b;
-b=a-b;
-a=a-b;
-printf("Swap is %d %d",a,b);
+
+int main(void)
+{ 
+	int a,b;
+	printf("Enter two Numbers : ");
+	scanf("%d %d",&a,&b);
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	printf("Swap is %d %d\n",a,b);
+	return 0;
 }
