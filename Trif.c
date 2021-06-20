5b9bdb41c9ee0dbaea7d77345573bd7016008ac9
lazy-dude <76253503+lazy-dude@users.noreply.github.com>
2021-06-20 12:28:31 +0430

    Update Trif.c

--- Trif.c
+++ Trif.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 #include<math.h>
-void main()
-{ double a,b;
-printf("Enter the degree : ");
-scanf("%lf",&a);
-a=(a*3.14)/180;
-b=sin(a);
-printf("Sine is %lf",b);
+
+int main(void)
+{ 
+	double a,b;
+	printf("Enter the degree : ");
+	scanf("%lf",&a);
+	a=(a*3.14)/180;
+	b=sin(a);
+	printf("Sine is %lf",b);
+	return 0;
 }
