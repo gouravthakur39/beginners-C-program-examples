65829600abd7b2859002f1a4f5a45c98b575ba6f
lazy-dude <atorrses@gmail.com>
2021-07-05 14:48:39 +0430

    defined PI

--- AreaAndCircumference.c
+++ AreaAndCircumference.c
@@ -1,13 +1,16 @@
 // To calculate Area and Circumference of a circle
 
 #include<stdio.h>
-int main()
+
+#define PI 3.14
+
+int main(void)
 {
     float r, a ,c;
     printf("enter radius :\n");
     scanf("%f", &r);
-    a = 3.14*r*r;
-    c = 2*3.14*r;
+    a = PI*r*r;
+    c = 2*PI*r;
     printf("Area = %f\n circumference = %f\n",a,c);
     return 0;
 }
