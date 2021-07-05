5893ea23c478467956ec25d5236e417116341ec5
lazy-dude <atorrses@gmail.com>
2021-07-05 17:13:09 +0430

    int to unsigned for bitwise operations

--- BitwiseComplementOperator.c
+++ BitwiseComplementOperator.c
@@ -1,11 +1,12 @@
 // Bitwise complement operator
 
 #include <stdio.h>
+
 int main(void)
 {
-    int a=14, b;
+	// usually usigned values are used to bitwise opeations
+    unsigned a=14, b;
     b = ~a;
-    printf("Value of c = %d\n",b);
+    printf("Value of c = %u\n",b);
     return 0;
-
 }
