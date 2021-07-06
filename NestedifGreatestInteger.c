295c5e02c20db741b686fe0b0358ebdd04ab8a12
lazy-dude <atorrses@gmail.com>
2021-07-06 17:30:14 +0430

    Auto indented

--- NestedifGreatestInteger.c
+++ NestedifGreatestInteger.c
@@ -2,20 +2,20 @@
 
 #include<stdio.h>
 
-int main()
-{
-    int a,b,c;
-    printf("enter three integer number\n");
-    scanf("%d%d%d",&a,&b,&c);
-    if(a>b)
-    if(a>c)
-        printf("a is greatest");
+int main(void) {
+  int a, b, c;
+  printf("enter three integer number\n");
+  scanf("%d%d%d", &a, &b, &c);
+  if (a > b)
+    if (a > c)
+      printf("a is greatest");
     else
-        printf("c is greatest");
-    else
-    if(b>c)
-        printf("b is greatest");
-    else
-        printf("c is greatest");
-    return 0;
+      printf("c is greatest");
+  else if (b > c)
+    printf("b is greatest");
+  else
+    printf("c is greatest");
+    
+  printf("\n");
+  return 0;
 }
