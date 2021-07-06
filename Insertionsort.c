74f20f7ae73acfdbf286316c54133100a26ca363
lazy-dude <atorrses@gmail.com>
2021-07-06 16:56:06 +0430

    Indented code

--- Insertionsort.c
+++ Insertionsort.c
@@ -2,35 +2,35 @@
 
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-  int n, array[1000], c, d, t;
+	int n, array[1000], c, d, t;
 
-  printf("Enter number of elements\n");
-  scanf("%d", &n);
+	printf("Enter number of elements\n");
+	scanf("%d", &n);
 
-  printf("Enter %d integers\n", n);
+	printf("Enter %d integers\n", n);
 
-  for (c = 0; c < n; c++)
-    scanf("%d", &array[c]);
+	for (c = 0; c < n; c++)
+    	scanf("%d", &array[c]);
 
-  for (c = 1 ; c <= n - 1; c++) {
-    d = c;
+	for (c = 1 ; c <= n - 1; c++) {
+    	d = c;
 
-    while ( d > 0 && array[d-1] > array[d]) {
-      t          = array[d];
-      array[d]   = array[d-1];
-      array[d-1] = t;
+    	while ( d > 0 && array[d-1] > array[d]) {
+      		t          = array[d];
+      		array[d]   = array[d-1];
+      		array[d-1] = t;
 
-      d--;
-    }
-  }
+      		d--;
+    	}
+	}
 
-  printf("Sorted list in ascending order:\n");
+	printf("Sorted list in ascending order:\n");
 
-  for (c = 0; c <= n - 1; c++) {
-    printf("%d\n", array[c]);
-  }
+	for (c = 0; c <= n - 1; c++) {
+    	printf("%d\n", array[c]);
+  	}
 
-  return 0;
+	return 0;
 }
