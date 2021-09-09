cfc7a13c0e62fc2bc2be3a02f65f89cc78a59aa2
THALLAPELLI JAYANTH <75982026+jayanth9676@users.noreply.github.com>
2021-09-09 16:56:09 +0530

    changed the description

--- swappingwithoutthirdvariable.c
+++ swappingwithoutthirdvariable.c
@@ -5,10 +5,11 @@ int main()
     int a,b;  /*declaration of variables*/
     printf("Enter Two Numbers\n");
     scanf("%d %d",&a,&b); /*Taking input of Two Variables*/
-  /*  a=a+b; //swapping 
-    b=a-b; // the two 
-    a=a-b; //numbers    */
-    b=a+b-(a=b);
+    b = a + b - (a = b);    /*  firstly a is assigned with b in "(a = b)" ,
+                            and then it becomes "a + b - b" ==> "a" , i.e, "b = a + b - b " 
+                            finally left-side b assigned with a , "b = a"
+                            Hence they get swapped!
+                         */
     printf("the number after swapping are %d %d\n",a,b);/*printing*/
     return 0;
 }
