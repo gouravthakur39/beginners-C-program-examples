f75c9c8762762cf2b0297a48a3af879dc04cb8cd
Sivasankar-03 <91017326+Sivasankar-03@users.noreply.github.com>
2021-09-30 19:30:55 +0530

    Single line

--- Addition.c
+++ Addition.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 void main()
-{ int a,b,c;
+{ int a,b;
 printf("Enter two Numbers : ");
 scanf("%d %d",&a,&b);
-c=a+b;
-printf("The Sum is %d",c);
+
+printf("The Sum is %d",a+b);
 }
