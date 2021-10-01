971274a90d03cf21f6b9bd6492114824b7875329
Nihal Kumar Das <56214697+Nihal0511@users.noreply.github.com>
2021-10-01 13:19:55 +0530

    Calculated the factorial using recursion

--- Factorial.c
+++ Factorial.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
+int fact(int);
 int main()
 {
-    int n, i;
+    int n;
     unsigned long long factorial = 1;
     //unsigned long long is the same as unsigned long long int. 
     //Its size is platform-dependent, but guaranteed by the C standard (ISO C99) to be at least 64 bits. 
 
-    printf("Enter a number: ");
+    printf("Enter a positive integer number: ");
     scanf("%d",&n);
 
-    // Show error if number is less than 0
-    if (n < 0)
-        printf("Error! Factorial of a negative number doesn't exist.");
-
-    else
+   printf("%d\n",fact(n));
+    int fact(int n)
     {
-        for(i=1; i<=n; ++i)
-        {
-            factorial *= i;              // factorial = factorial*i;
-        }
-        printf("Factorial of %d = %llu", n, factorial);
+        if(n==0)
+            return(1);
+        else 
+            return(n*fact(n-1));
     }
-
+   
     return 0;
 }
