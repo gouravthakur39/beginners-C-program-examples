d017698e579bd714a032dbc9e20c14fb387fbe9b
QubitMatrix <60323193+QubitMatrix@users.noreply.github.com>
2022-10-16 00:32:00 +0530

    Matrix multiplication(using DMA2)
    Matrix multiplication using dynamic memory allocation(using double pointers, i.e pointer of pointer)

--- matrixmultiplication.c
+++ matrixmultiplication.c
@@ -1,58 +1,66 @@
-
-//DMA:using single pointer
-#include<stdio.h>
+//DMA:using double pointer
+ #include<stdio.h>
  #include<stdlib.h>
-void mul(int *a,int *b,int *c,int r1,int c1,int c2)//single pointers used
+void mul(int **a,int **b,int **c,int r1,int c1,int c2)//same as using sma but the notations differ
 {
-    for(int i=0;i<r1;i++)
-    {
-        for(int j=0;j<c2;j++)
-        {
-            *(c+(i*c2)+j)=0;
-            for(int k=0;k<c1;k++)
-            {
-                *(c+(i*c2)+j)+=*(a+(i*c1)+k)*(*(b+(k*c2)+j));
-            }
-        }
-    }
+	for(int i=0;i<r1;i++)
+	{
+		for(int j=0;j<c2;j++)
+		{
+			c[i][j]=0;
+			for(int k=0;k<c1;k++)
+			{
+				*(*(c+i)+j)+=*(*(a+i)+k)*(*(*(b+k)+j));
+			}
+		}
+	}
 }
-int* allocat(int r,int c)//dynamic allocations
+int** allocat(int r,int c)
 {
-    int *ptr;
-    ptr=malloc(r*c*sizeof(int));
-    return ptr;
+	int **ptr;
+	ptr=malloc(r*sizeof(int*));//dynamically allocate memory for pointer of pointer
+	for(int i=0;i<c;i++)
+	{
+		ptr[i]=malloc(c*sizeof(int));//dynamically allocate memory for each pointer
+	}
+	return ptr;
 }
-void inp(int *ptr,int r,int c)//inputs
+void inp(int **ptr,int r,int c)
 {
-    for(int i=0;i<r*c;i++)
-    {
-        scanf("%d",ptr++);//or ptr+i
-    }
+	for(int i=0;i<r;i++)
+	{
+		for(int j=0;j<c;j++)
+		{
+			scanf("%d",(*(ptr+i)+j));//take input elements
+		}
+	}
 }
-void out(int *ptr,int r,int c)//output the matrix
+void out(int **ptr,int r,int c)
 {
-    for(int i=0;i<r*c;i++)
-    {
-        printf("%d ",*(ptr++));
-        if(i%3==2)
-            printf("\n");
-    }
+	for(int i=0;i<r;i++)
+	{
+		for(int j=0;j<c;j++)
+		{
+			printf("%d ",*(*(ptr+i)+j));//output the multiplied matrix
+		}
+		printf("\n");
+	}
 }
 int main()
 {
-    int r1,c1,r2,c2;
-    int *ma,*mb,*mc;//single pointers used
-    printf("Enter the row1 col1 ro2 col2\n");
-    scanf("%d %d %d %d",&r1,&c1,&r2,&c2);
-    ma=allocat(r1,c1);//memory allocated dynamically
-    mb=allocat(r2,c2);
-    mc=allocat(r1,c2);
-    printf("Enter elements of first\n");
-    inp(ma,r1,c1);
-    printf("Enter elements of second\n");
-    inp(mb,r2,c2);
-    mul(ma,mb,mc,r1,c1,c2);//multiplied
-    printf("Multiplied matrix:\n");
-    out(mc,r1,c2);
-    return 0;
+	int r1,c1,r2,c2;
+	int **ma,**mb,**mc;//double pointers used
+	printf("Enter the row1 col1 ro2 col2\n");
+	scanf("%d %d %d %d",&r1,&c1,&r2,&c2);
+	ma=allocat(r1,c1);//first memory allocated for each
+	mb=allocat(r2,c2);
+	mc=allocat(r1,c2);
+	printf("Enter elements of first\n");
+	inp(ma,r1,c1);
+	printf("Enter elements of second\n");
+	inp(mb,r2,c2);
+	mul(ma,mb,mc,r1,c1,c2);//multiplied
+	printf("Multiplied matrix:\n");
+	out(mc,r1,c2);
+	return 0;
 }
