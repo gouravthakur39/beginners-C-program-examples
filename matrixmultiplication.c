3cf146ff99cebe106252a494ab22ab9d20be4452
QubitMatrix <60323193+QubitMatrix@users.noreply.github.com>
2022-10-16 00:30:49 +0530

    Matrix Multiplication(using DMA1)
    Matrix multiplication using dynamic memory allocation(single pointers used)

--- matrixmultiplication.c
+++ matrixmultiplication.c
@@ -1,52 +1,58 @@
-#include<stdio.h>
-void mul(int c1,int c2;int a[][c1],int b[][c2],int c[][c2],int r1,int c1,int r2,int c2)
-{
-	for(int i=0;i<r1;i++)//to move through rows in first
-	{
-		for(int j=0;j<c2;j++)//to move throught columns in second matrix
-		{
-			*(*(c+i)+j)=0;
-			for(int k=0;k<c1;k++)//access each in the row and the column and multiply and keep adding it
-			{
-				*(*(c+i)+j)+=*(*(a+i)+k)*(*(*(b+k)+j));
-			}
-		}
-	}
-}
 
-void inp(int c;int (*a)[c],int r,int c)
+//DMA:using single pointer
+#include<stdio.h>
+ #include<stdlib.h>
+void mul(int *a,int *b,int *c,int r1,int c1,int c2)//single pointers used
 {
-	for(int i=0;i<r;i++)
-	{
-		for(int j=0;j<c;j++)
-		{
-			scanf("%d",(*(a+i)+j));//takes elements in first matrix
-		}
-	}
+    for(int i=0;i<r1;i++)
+    {
+        for(int j=0;j<c2;j++)
+        {
+            *(c+(i*c2)+j)=0;
+            for(int k=0;k<c1;k++)
+            {
+                *(c+(i*c2)+j)+=*(a+(i*c1)+k)*(*(b+(k*c2)+j));
+            }
+        }
+    }
 }
-void out(int c;int a[][c],int r,int c)
+int* allocat(int r,int c)//dynamic allocations
 {
-	for(int i=0;i<r;i++)
-	{
-		for(int j=0;j<c;j++)
-		{
-			printf("%d ",*(*(a+i)+j));//takes elements in second matrix
-		}
-		printf("\n");
-	}
+    int *ptr;
+    ptr=malloc(r*c*sizeof(int));
+    return ptr;
+}
+void inp(int *ptr,int r,int c)//inputs
+{
+    for(int i=0;i<r*c;i++)
+    {
+        scanf("%d",ptr++);//or ptr+i
+    }
+}
+void out(int *ptr,int r,int c)//output the matrix
+{
+    for(int i=0;i<r*c;i++)
+    {
+        printf("%d ",*(ptr++));
+        if(i%3==2)
+            printf("\n");
+    }
 }
 int main()
 {
-	int r1,c1,r2,c2;
-	printf("Enter the row1 col1 ro2 col2\n");//assuming the multiplication rules are satisfied
-	scanf("%d %d %d %d",&r1,&c1,&r2,&c2);
-	int a[r1][c1];int b[r2][c2];int c[r1][c2];
-	printf("Enter elements of first\n");
-	inp(a,r1,c1);
-	printf("Enter elements of second\n");
-	inp(b,r2,c2);
-	mul(a,b,c,r1,c1,r2,c2);//mulitplies two matrices
-	printf("Multiplied matrix:\n");
-	out(c,r1,c2);//prints multiplied matrix
-	return 0;
+    int r1,c1,r2,c2;
+    int *ma,*mb,*mc;//single pointers used
+    printf("Enter the row1 col1 ro2 col2\n");
+    scanf("%d %d %d %d",&r1,&c1,&r2,&c2);
+    ma=allocat(r1,c1);//memory allocated dynamically
+    mb=allocat(r2,c2);
+    mc=allocat(r1,c2);
+    printf("Enter elements of first\n");
+    inp(ma,r1,c1);
+    printf("Enter elements of second\n");
+    inp(mb,r2,c2);
+    mul(ma,mb,mc,r1,c1,c2);//multiplied
+    printf("Multiplied matrix:\n");
+    out(mc,r1,c2);
+    return 0;
 }
