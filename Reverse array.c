7aa8db2f73e6b0117dbc99171385bd194d7ed80d
rachit7596 <116446357+rachit7596@users.noreply.github.com>
2022-10-27 22:59:56 +0530

    Reverse array.c
    Hacktoberfest-accepted

---
+++ Reverse array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define n 6
+int main(){
+    int arr[n] = {9, 8, 7, 2, 4, 3};
+    int temp;
+    for(int i = 0; i<n/2; i++){
+        temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
+    }
+    for(int i = 0; i < n; i++){
+        printf("%d,", arr[i]);
+    }
+}
