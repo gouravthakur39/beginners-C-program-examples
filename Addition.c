4e95c8c889d6ece4021f5842f2b2c50e4af18e99
Josel099 <115106938+Josel099@users.noreply.github.com>
2022-10-30 14:53:16 +0530

    Update Addition.c

--- Addition.c
+++ Addition.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 void main()
 { int a,b,c;
-printf("Enter two Numbers : ");
+ printf("*****calculating the sum of two numbers*****");
+printf("\nEnter two Numbers : ");
 scanf("%d %d",&a,&b);
 c=a+b;
-printf("The Sum is %d",c);
+printf("The Sum of the given numbers are: %d",c);
 }
