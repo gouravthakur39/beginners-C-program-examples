4656e543da4825f96d035a9158d5cd932a8de933
Hywoid <48028774+hywoid@users.noreply.github.com>
2022-11-29 08:34:12 +0300

    Update Addition.c
    fixed readability

--- Addition.c
+++ Addition.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
-void main()
-{ int a,b,c;
-printf("Enter two Numbers : ");
-scanf("%d %d",&a,&b);
-c=a+b;
-printf("The Sum is %d",c);
+#include <stdio.h>
+
+void main() { 
+  int a,b,c;
+  printf("Enter two Numbers : ");
+  scanf("%d %d",&a,&b);
+  c=a+b;
+  printf("The Sum is %d",c);
 }
